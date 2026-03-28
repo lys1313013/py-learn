c72e8f95b5986be088720443f68867ce4558a8b5
lys1313013 <lys1313013@gmail.com>
2026-03-28 23:04:55 +0800

    列式推导式

--- 5_循环/6_for_ListComprehension.py
+++ 5_循环/6_for_ListComprehension.py
@@ -4,3 +4,7 @@ num_list = [1, 2, 3, 4]
 
 num_list2 = [item * item for item in num_list]
 print(num_list2)
+
+# 列式推导式：加判断
+num_list3 = [item * item for item in num_list if item % 2 == 0]
+print(num_list3)
